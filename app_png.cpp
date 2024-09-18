cac9e42c0ab0b8c86f6227ffb21434bbe3c09015
mariuszmaximus <mariusz.maximus@gmail.com>
2024-09-18 22:56:43 +0200

    add files

---
+++ app_png.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <png.h>
+
+int main(int, char**){
+    std::cout << "Hello, from libpng_usage!\n"; 
+}
---
+++ app_z.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <zlib.h>
+
+int main(int, char**){
+    std::cout << "Hello, from libpng_usage!\n"; 
+}
